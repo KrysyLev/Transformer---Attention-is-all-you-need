8d2f76255edf6edf6cfcb488dc9f274d7b547ed8
Krysy Lev <119929320+KrysyLev@users.noreply.github.com>
2025-03-01 18:43:50 +0700

    Implement DecoderBlock, Decoder, and ProjectionLayer

--- model.py
+++ model.py
@@ -161,3 +161,37 @@ class Encoder(nn.Module):
         return self.norm(x) 
 #     The original Transformer paper (Attention is All You Need) does not apply normalization at the end.
 # But modern implementations (like fairseq, Hugging Face Transformers) do, because it improves stability.
+
+class DecoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiheadAttentionBlock, cross_attention_block: MultiheadAttentionBlock, feed_foward_block: FeedForwardBlock, dropout: float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_foward_block
+        self.residual_connections = nn.Module([ResidualConnection(dropout) for _ in range(3)])
+        
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connections[0](x, lambda x : self.self_attention_block(x, x, x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x : self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block)
+        return x
+    
+class Decoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+        
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
+
+class ProjectionLayer(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+    
+    def forward(self, x):
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, vocab_size)
+        return torch.log_softmax(self.proj(x), dim = -1)
