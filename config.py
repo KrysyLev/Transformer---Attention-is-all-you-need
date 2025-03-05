76417b3a11a84a0b2d44b668dd3f075e559adf4e
Krysy Lev <119929320+KrysyLev@users.noreply.github.com>
2025-03-05 16:16:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -12,19 +12,18 @@ def get_config():
         "lang_tgt": "it",
         "model_folder": "weights",
         "model_basename": "tmodel_",
-        "preload": None,
+        "preload": "latest",
         "tokenizer_file": "tokenizer_{0}.json",
         "experiment_name": "runs/tmodel",
-        "datasource": "opus_books",
+        "datasource": 'opus_books',
     }
 
 
-def get_weights_file_path(config, epoch: str):
+def get_weight_file_path(config, epoch: str):
     model_folder = config["model_folder"]
     model_basename = config["model_basename"]
     model_filename = f"{model_basename}{epoch}.pt"
     return str(Path(".") / model_folder / model_filename)
-
 
 # Find the latest weights file in the weights folder
 def latest_weights_file_path(config):
